a872f2a68e0e78069f4b7697bde5012a2e075b71
darvin7531 <srrafale@gmail.com>
2026-09-14 20:05:26 +0300

    test: cover persistent war state across round restart

---
+++ Content.IntegrationTests/Tests/GameRules/WarStatePersistenceTest.cs
@@ -0,0 +1,42 @@
+using System.IO;
+using System.Text.Json;
+using Content.IntegrationTests.Fixtures;
+using Content.Server.GameTicking;
+using Content.Server.War;
+using Robust.Shared.ContentPack;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Utility;
+
+namespace Content.IntegrationTests.Tests.GameRules;
+
+[TestFixture]
+public sealed class WarStatePersistenceTest : GameTest
+{
+    public override PoolSettings PoolSettings => new()
+    {
+        Connected = true,
+        Dirty = true,
+        DummyTicker = false,
+    };
+
+    [Test]
+    public async Task TechnicalRoundRestartPreservesWar()
+    {
+        var server = Pair.Server;
+        var resources = server.ResolveDependency<IResourceManager>();
+        var ticker = server.System<GameTicker>();
+        var war = server.System<WarStateSystem>();
+
+        await server.WaitAssertion(() =>
+        {
+            resources.UserData.Delete(WarStateSystem.SavePath);
+            var expected = war.StartNewWar();
+
+            ticker.RestartRound(force: true);
+
+            Assert.That(war.State, Is.EqualTo(expected));
+            using var stream = resources.UserData.Open(WarStateSystem.SavePath, FileMode.Open);
+            Assert.That(JsonSerializer.Deserialize<WarState>(stream), Is.EqualTo(expected));
+        });
+    }
+}
