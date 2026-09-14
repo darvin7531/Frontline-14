e269208744c115caac01fd93bd254b3a86ec47ac
darvin7531 <srrafale@gmail.com>
2026-09-14 18:14:49 +0300

    test: cover persistent war bootstrap behavior

---
+++ Content.IntegrationTests/Tests/GameRules/PersistentWarRuleTest.cs
@@ -0,0 +1,54 @@
+using Content.IntegrationTests.Fixtures;
+using Content.Server.GameTicking;
+using Content.Server.GameTicking.Rules.Components;
+using Content.Server.Ghost;
+using Content.Server.Mind;
+using Content.Server.Roles;
+using Content.Shared.GameTicking.Components;
+using Content.Shared.Roles.Components;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Player;
+
+namespace Content.IntegrationTests.Tests.GameRules;
+
+[TestFixture]
+[TestOf(typeof(PersistentWarRuleComponent))]
+public sealed class PersistentWarRuleTest : GameTest
+{
+    public override PoolSettings PoolSettings => new()
+    {
+        Connected = true,
+        Dirty = true,
+        DummyTicker = false,
+        InLobby = true,
+    };
+
+    [Test]
+    public async Task StartsWithoutJobRoleAndBlocksNormalGhosting()
+    {
+        var ticker = Server.System<GameTicker>();
+        var mindSystem = Server.System<MindSystem>();
+        var roleSystem = Server.System<RoleSystem>();
+        var ghostSystem = Server.System<GhostSystem>();
+
+        await Server.WaitPost(() =>
+        {
+            ticker.SetGamePreset("PersistentWar");
+            ticker.ToggleReadyAll(true);
+            ticker.StartRound(true);
+        });
+        await Pair.RunUntilSynced();
+
+        await Server.WaitAssertion(() =>
+        {
+            Assert.That(ticker.RunLevel, Is.EqualTo(GameRunLevel.InRound));
+            Assert.That(ticker.CurrentPreset?.ID, Is.EqualTo("PersistentWar"));
+            Assert.That(SEntMan.Count<ActiveGameRuleComponent>(), Is.EqualTo(1));
+            Assert.That(SEntMan.Count<PersistentWarRuleComponent>(), Is.EqualTo(1));
+
+            Assert.That(mindSystem.TryGetMind(ServerSession!, out var mindId, out var mind));
+            Assert.That(roleSystem.MindHasRole<JobRoleComponent>(mindId), Is.False);
+            Assert.That(ghostSystem.OnGhostAttempt(mindId, true, viaCommand: true, mind: mind), Is.False);
+        });
+    }
+}
