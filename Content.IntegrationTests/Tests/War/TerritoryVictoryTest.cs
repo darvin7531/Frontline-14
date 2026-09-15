7e18c3cc262e93fa0527c335812f72f69f21cdb9
darvin7531 <srrafale@gmail.com>
2026-09-15 21:09:42 +0300

    test: define territory victory threshold

---
+++ Content.IntegrationTests/Tests/War/TerritoryVictoryTest.cs
@@ -0,0 +1,47 @@
+using System.Numerics;
+using Content.IntegrationTests.Fixtures;
+using Content.Server.War;
+using Content.Shared.War;
+using Robust.Shared.GameObjects;
+
+namespace Content.IntegrationTests.Tests.War;
+
+[TestFixture]
+public sealed class TerritoryVictoryTest : GameTest
+{
+    [Test]
+    public async Task FourOwnedTerritoriesEndWarForTheirFaction()
+    {
+        var server = Pair.Server;
+        var map = await Pair.CreateTestMap();
+        var faction = new FactionId("FrontlineFactionOne");
+        var war = server.System<WarStateSystem>();
+        var victory = server.System<WarVictorySystem>();
+
+        await server.WaitPost(() =>
+        {
+            war.StartNewWar();
+
+            for (var i = 0; i < 5; i++)
+            {
+                var coordinates = new EntityCoordinates(map.GridUid, new Vector2(i * 2, 0));
+                var territory = SEntMan.SpawnEntity(null, coordinates);
+                SEntMan.AddComponent<TerritoryComponent>(territory)
+                    .Configure(new TerritoryId($"territory-{i}"), coordinates.Position - new Vector2(0.5f), coordinates.Position + new Vector2(0.5f));
+
+                if (i == 4)
+                    continue;
+
+                var hall = SEntMan.SpawnEntity(null, coordinates);
+                SEntMan.AddComponent<TownHallComponent>(hall).Configure(new TerritoryId($"territory-{i}"), faction);
+            }
+        });
+
+        await server.WaitAssertion(() =>
+        {
+            Assert.That(victory.CheckForVictory(), Is.True);
+            Assert.That(war.State?.Status, Is.EqualTo(WarStatus.Ended));
+            Assert.That(war.State?.Winner, Is.EqualTo(faction));
+        });
+    }
+}
