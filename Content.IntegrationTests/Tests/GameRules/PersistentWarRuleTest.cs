8760202df72f008aec999737edd023f580b57cae
darvin7531 <srrafale@gmail.com>
2026-09-16 15:32:09 +0300

    test: specify persistent war ground cycle

--- Content.IntegrationTests/Tests/GameRules/PersistentWarRuleTest.cs
+++ Content.IntegrationTests/Tests/GameRules/PersistentWarRuleTest.cs
@@ -1,13 +1,23 @@
+using System;
+using System.IO;
+using System.Text.Json;
 using Content.IntegrationTests.Fixtures;
 using Content.IntegrationTests.Fixtures.Attributes;
 using Content.Server.GameTicking;
 using Content.Server.GameTicking.Presets;
 using Content.Server.GameTicking.Rules.Components;
 using Content.Server.Mind;
+using Content.Server.War;
+using Content.Shared.Atmos;
+using Content.Shared.Atmos.Components;
 using Content.Shared.CCVar;
 using Content.Shared.GameTicking.Components;
+using Content.Shared.Light.Components;
+using Content.Shared.Light.EntitySystems;
 
+using Robust.Shared.ContentPack;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Map.Components;
 using Robust.Shared.Player;
 
 namespace Content.IntegrationTests.Tests.GameRules;
@@ -26,12 +36,22 @@ public sealed class PersistentWarRuleTest : GameTest
 
     [Test]
     [EnsureCVar(Side.Server, typeof(CCVars), nameof(CCVars.GameMap), "")]
-    public async Task DoesNotStartWithoutConfiguredMap()
+    public async Task StartsGroundMapWithPersistentDayNight()
     {
         var ticker = Server.System<GameTicker>();
+        var resources = Server.ResolveDependency<IResourceManager>();
+        var war = Server.System<WarStateSystem>();
+        var startedAt = DateTimeOffset.UtcNow - TimeSpan.FromMinutes(7);
+        EntityUid map = default;
+        LightCycleComponent cycle = default!;
 
         await Server.WaitPost(() =>
         {
+            resources.UserData.Delete(WarStateSystem.SavePath);
+            using (var stream = resources.UserData.OpenWrite(WarStateSystem.SavePath))
+                JsonSerializer.Serialize(stream, new WarState(1, WarStatus.Active, startedAt));
+
+            ticker.RestartRound();
             ticker.SetGamePreset("PersistentWar");
             ticker.ToggleReadyAll(true);
             ticker.StartRound(true);
@@ -40,7 +60,40 @@ public sealed class PersistentWarRuleTest : GameTest
 
         await Server.WaitAssertion(() =>
         {
-            Assert.That(ticker.RunLevel, Is.EqualTo(GameRunLevel.PreRoundLobby));
+            Assert.That(ticker.RunLevel, Is.EqualTo(GameRunLevel.InRound));
+            Assert.That(war.State, Is.EqualTo(new WarState(1, WarStatus.Active, startedAt)));
+
+            map = Server.System<SharedMapSystem>().GetMapOrInvalid(ticker.DefaultMap);
+            var atmos = SEntMan.GetComponent<MapAtmosphereComponent>(map);
+            var light = SEntMan.GetComponent<MapLightComponent>(map);
+            cycle = SEntMan.GetComponent<LightCycleComponent>(map);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(atmos.Space, Is.False);
+                Assert.That(atmos.Mixture.GetMoles(Gas.Oxygen), Is.GreaterThan(0));
+                Assert.That(atmos.Mixture.GetMoles(Gas.Nitrogen), Is.GreaterThan(0));
+                Assert.That(cycle.Duration, Is.GreaterThan(TimeSpan.Zero));
+                Assert.That(SharedLightCycleSystem.GetColor((map, cycle), light.AmbientLightColor, 0),
+                    Is.Not.EqualTo(SharedLightCycleSystem.GetColor((map, cycle), light.AmbientLightColor, (float) cycle.Duration.TotalSeconds / 2)));
+            });
+
+            var expectedOffset = TimeSpan.FromTicks((DateTimeOffset.UtcNow - startedAt).Ticks % cycle.Duration.Ticks);
+            Assert.That((cycle.Offset - expectedOffset).Duration(), Is.LessThan(TimeSpan.FromSeconds(2)));
+
+            var validator = Server.System<PersistentWarMapValidatorSystem>();
+            Assert.DoesNotThrow(() => validator.Validate(map));
+            atmos.Space = true;
+            var error = Assert.Throws<InvalidOperationException>(() => validator.Validate(map));
+            Assert.That(error!.Message, Is.EqualTo("PersistentWar map must set MapAtmosphere.space to false."));
+            atmos.Space = false;
+        });
+
+        await Pair.RunUntilSynced();
+        await Pair.Client.WaitAssertion(() =>
+        {
+            var clientCycle = CEntMan.GetComponent<LightCycleComponent>(Pair.ToClientUid(map));
+            Assert.That(clientCycle.Offset, Is.EqualTo(cycle.Offset));
         });
 
         ticker.SetGamePreset((GamePresetPrototype) null);
